a0c2ba9fcff89cd68e4c656eae96d8d51fb73709
will9183 <26502724+will9183@users.noreply.github.com>
2020-07-22 16:09:43 -0400

    Redid the backend and made it one singular file using classes

--- Program/pistagemanager-gui.py
+++ Program/pistagemanager-gui.py
@@ -2,15 +2,14 @@ from guizero import App, Text, MenuBar, info, PushButton, ListBox, Box
 from tkinter import filedialog as fd
 import sys
 import webbrowser
-import show_load #from show_load.py
-import show_control #from show_control.py
-import interrupt_load #from interrupt_load.py
-
+from psmbackend import *
 
 # Variables
-show_directory = "Please Select a Show Directory!"
-interrupt_list = interrupt_load.load_interrupts() # List of interrupts in the interrupts folder
+loader = ShowLoader()
+controller = ShowController()
 
+show_directory = "Please Select a Show Directory!"
+interrupt_list = loader.interrupt_loader() # List of interrupts in the interrupts folder
 
 # Definitions
 # Exit Command
@@ -20,27 +19,27 @@ def exit_menu_option():
 # Open Show Command
 def open_show():
     global show_directory
-    show_directory = fd.askdirectory(initialdir = "/home", title="Please select the show folder") #Presents a diolog box to select a folder and save the directory to a variable
+    show_directory = fd.askdirectory(initialdir = "/home/pi", title="Please select the show folder") #Presents a diolog box to select a folder and save the directory to a variable
     show_folder_name.clear() #Clears the text at the top of the window
     show_folder_name.append("Show Folder: " + show_directory) #Changes the text with the show directory folder
-    show_load.load(show_directory) #Calls the load function in show_load.py to initialize the show
+    loader.show_loader(show_directory) #Calls the load function in show_load.py to initialize the show
 
 # Command for the back button
 def back_command():
-    show_control.cue_backward() #Call the cue_backward function in show_control.py
+    controller.back() #Call the cue_backward function in show_control.py
     cue_number.clear() #Clears the cue text
-    cue_number.append("Cue: " + str(show_control.current_cue)) #Changes the cue text to current cue number
+    cue_number.append("Cue: " + str(controller.current_cue)) #Changes the cue text to current cue number
 
 # Command for the forward button, same as the back button but calls the forward command instread
 def forward_command():
-    show_control.cue_forward()
+    controller.forward()
     cue_number.clear()
-    cue_number.append("Cue: " + str(show_control.current_cue))
+    cue_number.append("Cue: " + str(controller.current_cue))
 
 # Send Interrupt Command
 def send_interrupt():
     selected_interrupt = interrupt_listbox.value #adds the current interrupt selected in the box to the variable selected_interrupt
-    show_control.interrupt_show(selected_interrupt) #calls the interrupt_show function in show_control.py with the selected_interrupt as the parameter
+    controller.interrupt(selected_interrupt) #calls the interrupt_show function in show_control.py with the selected_interrupt as the parameter
 
 # About command
 def about():
@@ -81,7 +80,8 @@ interrupt_box = Box(app, width="fill", align="bottom", border=True)
 interrupt_text = Text(interrupt_box, text="Interrupts:")
 interrupt_listbox = ListBox(interrupt_box, items=interrupt_list, width="fill")
 interrupt_send = PushButton(interrupt_box, text="Send Selected Interrupt", command=send_interrupt, align="right")
-interrupt_stop = PushButton(interrupt_box, text="Stop Interrupt", command=show_control.stop_interrupt, align="left")
+interrupt_stop = PushButton(interrupt_box, text="Stop Interrupt", command=controller.set, align="left")
+#debug_button = PushButton(app, text="Debug", command=debug)
 
 
 app.display()
